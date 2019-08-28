500a766ffe9b9c6d5eac88b97e940fd13cf9c772
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-28 15:33:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    unless @roster[grade]
+      @roster[grade] = []
+    end
+    @roster[grade].push(name)
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.sort.to_h
+  end
+  
+  
+  
+
+    
+end
